c16d96504ee7b20175560a3fe0e23d0f083cdf80
Brittany Wood <bwood8@gatech.edu>
2013-06-25 17:02:43 -0400

    added CountryUtil.java

---
+++ src/main/java/edu/gatech/cs2340/risk/util/CountryUtil.java
@@ -0,0 +1,26 @@
+package main.java.edu.gatech.cs2340.risk.util;
+
+import java.util.ArrayList;
+
+import main.java.edu.gatech.cs2340.risk.model.*;
+
+/**
+ * @author Brittany Wood
+ *
+ */
+public class CountryUtil {
+
+	public static Player getOwner(Country country){
+		ArrayList<Territory> territories = country.getTerritories();
+		Player owner = territories[0].getOwner();
+		for (Territory territory : territories) {
+			if (owner.equals(territory.getOwner())){
+				owner = territory.getOwner();
+			} else {
+				owner = null;
+				break;
+			}
+		}
+		return owner;
+	} 
+}
