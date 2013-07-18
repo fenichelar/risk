5bf451ee7fab949a979febd86911e8353ed0c36e
Alec Fenichel <fenichelar@gatech.edu>
2013-07-18 17:02:21 -0400

    Revert "Merge pull request #79 from cepaulus/M4"
    This reverts commit 4f907ec8b91bc93c89d86fe58bc18b76000938b0, reversing
    changes made to 2448a1f9b48423ff224d56d002346b48ea3fcb62.

--- src/main/java/edu/gatech/cs2340/risk/util/RiskConstants.java
+++ src/main/java/edu/gatech/cs2340/risk/util/RiskConstants.java
@@ -2,32 +2,9 @@ package main.java.edu.gatech.cs2340.risk.util;
 
 public interface RiskConstants {
 	
-	// stages
-	public static final int INITIALIZE = 1;
-	public static final int ROTATE_PLAYERS = 2;
-	public static final int ATTACK = 3;
-	public static final int FORTIFY = 4;
-	public static final int SELECT_OPTIONS = 5;
-	
-	public static final int NO_STEP = 0;
-	
-	// steps corresponding to stage 2 (rotate players)
-	public static final int BEGINNING_OF_TURN = 1;
-	public static final int DURING_TURN = 2;
-	
-	// steps corresponding to stage 3 (attack)
-	public static final int SELECT_ATTACKING_TERRITORY = 1;
-	public static final int SELECT_DEFENDING_TERRITORY = 2;
-	public static final int SELECT_DEFENDING_ARMIES = 3;
-	public static final int DO_ATTACK = 4;
-	
-	// steps corresponding to stage 4 (fortify)
-	public static final int SELECT_FORTIFYING_TERRITORY = 1;
-	public static final int SELECT_FORTIFIED_TERRITORY = 2;
-	public static final int DO_FORTIFY = 3;
-
-	// direction numbers
-	public static final int NO_DIRECTION = 0;
-	public static final int INITIAL_DIRECTION = 1;
-	public static final int ADDITIONAL_ARMIES_DIRECTIONS = 2;
+	public static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	public static final String DB_NAME = "Risk_Database";
+	public static final String DB_URL = "jdbc:mysql://localhost/";
+	public static final String DB_USER = "root";
+	public static final String DB_PASSWORD = "mypass";
 }
